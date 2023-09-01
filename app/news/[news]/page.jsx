e5a4351d03a85e54a7e465d96fc129b838355fe9
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-09-01 21:05:49 +0300

    Fixed [news].js, added styles and displaying content

--- app/news/[news]/page.jsx
+++ app/news/[news]/page.jsx
@@ -1,11 +1,16 @@
 import { fetcher } from "@/lib/api"
+import { markdownToHTML } from "@/lib/markdownToHTML";
+import Link from "next/link";
 
 export default async function SingleNewsPage({params}) {
-    const newsResponse = await fetcher(`${process.env.STRAPI_API_URL}/slugify/slugs/newss/${params.news}?populate=*`)
-    await console.log(newsResponse);
+    const newsResponse = await fetcher(`${process.env.STRAPI_API_URL}/slugify/slugs/news/${params.news}?populate=*`)
+    const description = await markdownToHTML(newsResponse.data.attributes.description);
     return (
-        <main className="container mx-auto text-[black]">
-            Новина {params.news}
+        <main className="container mx-auto text-[black] flex flex-col p-7 font-probapro">
+            <h1 className="w-full text-center text-2xl mb-4">{newsResponse.data.attributes.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: description}} className="dangerously-set-news">
+                {/* DESCRIPTION FROM NEWS */}
+            </div>
         </main>
     )
 }
