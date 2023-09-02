f49e7785500dcd4b982643361e4c9f56d896399f
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-09-02 22:38:21 +0300

    Created client components for fontawesomeicons, added displaying part of content of head_of_department

---
+++ app/components/fontAwesomeClientComponents/Email.jsx
@@ -0,0 +1,11 @@
+"use client"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+ 
+
+export default function FontAwesomeEmail() {
+    return (
+        <FontAwesomeIcon icon={faEnvelope} />
+    )
+}
--- app/departments/[department]/page.jsx
+++ app/departments/[department]/page.jsx
@@ -1,9 +1,17 @@
+import FontAwesomeEmail from "@/app/components/fontAwesomeClientComponents/Email";
+import FontAwesomePhone from "@/app/components/fontAwesomeClientComponents/Phone";
 import { fetcher } from "@/lib/api";
 import { markdownToHTML } from "@/lib/markdownToHTML";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Department({params}) {
-    const departmentData = await fetcher(`${process.env.STRAPI_API_URL}/slugify/slugs/department/${params.department}`)
-    console.log(departmentData);
+    const departmentData = await fetcher(`${process.env.STRAPI_API_URL}/slugify/slugs/department/${params.department}?populate=head_of_department&populate=head_of_department.photo`)
+    const headOfDepartment = departmentData.data.attributes.head_of_department.data.attributes
+    const headOfDepartmentPhoto = headOfDepartment.photo.data.attributes
+    console.log(headOfDepartmentPhoto);
     const description = await markdownToHTML(departmentData.data.attributes.description)
     return (
         <main className="container mx-auto text-[black] font-probapro flex flex-col py-8">
@@ -11,6 +19,21 @@ export default async function Department({params}) {
                 {departmentData.data.attributes.title}
             </h1>
             <div dangerouslySetInnerHTML={{__html: description}} className="dangerously-set-news"></div>
+            <h2 className="text-center font-semibold text-2xl text-headerSecond my-6">Завідуючий відділенням</h2>
+            <div className="flex flex-row justify-evenly">
+                <div className="flex flex-col font-probaprosmbd text-headerSecond gap-y-3">
+                    <h2 className="text-headerFirst text-xl mb-4 ">{headOfDepartment.name}</h2>
+                    {headOfDepartment.contacts.tel.map(tel => <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomePhone />
+                        <Link href={`tel:${tel}`} style={{letterSpacing: '1px'}}>{tel}</Link>
+                    </div>)}
+                    <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomeEmail />
+                        <Link href={`mailto:${headOfDepartment.contacts.email}`}>{headOfDepartment.contacts.email}</Link>
+                    </div>
+                </div>
+                <Image src={`${process.env.STRAPI_URL}${headOfDepartmentPhoto.url}`} width={300} height={400} alt={headOfDepartmentPhoto.name} />
+            </div>
         </main>
     )
 }
