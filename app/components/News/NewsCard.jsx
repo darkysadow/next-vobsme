cd807304ed7891bcc34bfaa8f8d6784d1fd52813
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-09-03 01:02:26 +0300

    Added adaptive layout to news and news card component, created FontAwesomeNewspaper client component (but i have a hydration error)

--- app/components/News/NewsCard.jsx
+++ app/components/News/NewsCard.jsx
@@ -1,19 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
+import FontAwesomeNewspaper from "../fontAwesomeClientComponents/Newspaper";
 
-export default function NewsCard ({id, item}) {
-    return(
-        <div key={id} className='w-[30%] flex flex-col items-start justify-start shadow-xl p-2 relative pb-11'>
-            <Image 
-                src={`${process.env.STRAPI_URL}${item.previewImage.data.attributes.url}`} 
-                width={300}
-                height={item.previewImage.data.attributes.height} 
-                alt={item.previewImage.data.attributes.alternativeText ? item.previewImage.data.attributes.alternativeText : 'Зображення '}
-                style={{objectFit: "cover", backgroundColor: 'rgba(12,12,12,0.3)', height: '200px'}}
-            />
-            <h2 className="font-semibold">{item.title}</h2>
-            <p className="line-clamp-4">{item.description}</p>
-            <Link href={`news/${item.slug}`} className="absolute bottom-2 right-5 hover:text-headerFirst">Перейти до новини →</Link>
+export default function NewsCard({ id, item }) {
+    return (
+        <div className="relative w-[30%] max-md:w-[45%] max-[492px]:w-[100%] flex flex-col items-start justify-start shadow-xl p-2 pb-11 font-probapro">
+        
+                {item.previewImage.data ?
+                    <Image
+                        src={`${process.env.STRAPI_URL}${item.previewImage.data.attributes.url}`}
+                        width={300}
+                        height={item.previewImage.data.attributes.height}
+                        alt={item.previewImage.data.attributes.alternativeText ? item.previewImage.data.attributes.alternativeText : 'Зображення '}
+                        style={{ objectFit: "cover", backgroundColor: 'rgba(12,12,12,0.3)', height: '200px' }}
+                        className="max-[492px]:mx-auto"
+                    /> :
+                    <div className="h-[200px] w-[100%] flex justify-center items-center">
+                        <FontAwesomeNewspaper color={"gray"} size={'2xl'} />
+                    </div>
+                }
+                <Link href={`news/${item.slug}`}>
+                <h2 className="font-semibold">{item.title}</h2>
+                <p className="line-clamp-4 w-[100%]">{item.description}</p>
+                <Link href={`news/${item.slug}`} className="absolute bottom-2 right-5 hover:text-headerFirst text-headerSecond font-semibold">Перейти до новини</Link>
+        </Link>
         </div>
     )
 }
---
+++ app/components/fontAwesomeClientComponents/Newspaper.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { faNewspaper } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export default function FontAwesomeNewspaper(props) {
+    return (
+        <FontAwesomeIcon icon={faNewspaper} {...props} />
+    )
+}
