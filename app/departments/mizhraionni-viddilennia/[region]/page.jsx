7cf88f6a3c349c4fa27a3dccd446da64a29b3452
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-09-05 11:22:06 +0300

    created nested route region in mizhraionni-viddilennia

---
+++ app/departments/mizhraionni-viddilennia/[region]/page.jsx
@@ -0,0 +1,14 @@
+import { fetcher } from "@/lib/api";
+import { markdownToHTML } from "@/lib/markdownToHTML";
+
+export default async function Region({params}) {
+    const depRes = await fetcher(`${process.env.STRAPI_API_URL}/slugify/slugs/idepartment/${params.region}?populate=head_of_department&populate=head_of_department.photo`)
+    const headOfDepartment = depRes.data.attributes.head_of_department.data ? depRes.data.attributes.head_of_department.data.attributes : undefined
+    const headOfDepartmentPhoto = headOfDepartment !== undefined && headOfDepartment.photo.data.attributes
+    const description = await markdownToHTML(depRes.data.attributes.description)
+    return (
+        <main className="container mx-auto text-[black]">
+            region {params.region}
+        </main>
+    )
+}
