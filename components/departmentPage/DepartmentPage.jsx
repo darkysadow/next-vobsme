8d0b87f97d5c74993481655198a1b29d2098e0fb
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-09-05 12:02:52 +0300

    Created department page component

---
+++ components/departmentPage/DepartmentPage.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import FontAwesomeEmail from "../fontAwesomeClientComponents/Email";
+import FontAwesomePhone from "../fontAwesomeClientComponents/Phone";
+import Image from "next/image";
+
+export default function DepartmentPage({departmentData, headOfDepartment, headOfDepartmentPhoto, description}) {
+    console.log(departmentData.data.attributes);
+    return (
+        <main className="container mx-auto px-5 text-[black] font-probapro flex flex-col py-8">
+            <h1 className="text-center font-semibold text-2xl text-headerSecond mb-6">
+                {departmentData.data.attributes.title}
+            </h1>
+            <div dangerouslySetInnerHTML={{__html: description}} className="dangerously-set-department"></div>
+            
+            {departmentData.data.attributes.head_of_department.data && 
+            <>
+            <h2 className="text-center font-semibold text-2xl text-headerSecond my-6">
+                Завідуючий {departmentData.data.attributes.title.toLowerCase().includes('відділення') ? "відділенням" : "відділом"}
+            </h2>
+            <div className="flex flex-row justify-evenly">
+                <div className="flex flex-col font-probaprosmbd text-headerSecond gap-y-3">
+                    <h2 className="text-headerFirst text-xl mb-4 ">{headOfDepartment.name}</h2>
+                    {headOfDepartment.contacts.tel && headOfDepartment.contacts.tel.map(tel => <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomePhone />
+                        <Link href={`tel:${tel}`} style={{letterSpacing: '1px'}}>{tel}</Link>
+                    </div>)}
+                    <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomeEmail />
+                        <Link href={`mailto:${headOfDepartment.contacts.email}`}>{headOfDepartment.contacts.email}</Link>
+                    </div>
+                </div>
+                <Image src={`${process.env.STRAPI_URL}${headOfDepartmentPhoto.url}`} width={300} height={400} alt={headOfDepartmentPhoto.name} />
+            </div></>}
+        </main>
+    )
+}
