4842bc83bdcaf5cfe3886c4e9a131ef3c1c9b6bd
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-09-05 12:03:55 +0300

    added loading skeletons to region page

---
+++ app/departments/mizhraionni-viddilennia/[region]/loading.jsx
@@ -0,0 +1,26 @@
+import { Skeleton } from "@/components/Skeleton";
+
+
+export default function RegionLoading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (<div className="container mx-auto px-5">
+        <Skeleton className="h-4 w-[40%] mx-auto my-8" />
+        <Skeleton className="h-4 w-[95%] my-2" />
+        <Skeleton className="h-4 w-[92%] my-2" />
+        <Skeleton className="h-4 w-[98%] my-2" />
+        <Skeleton className="h-4 w-[90%] my-2" />
+        <Skeleton className="h-4 w-[40%] mx-auto my-8" />
+        <Skeleton className="h-4 w-[90%] my-2" />
+        <Skeleton className="h-4 w-[95%] my-2" />
+        <Skeleton className="h-4 w-[97%] my-2" />
+        <Skeleton className="h-4 w-[91%] my-2" />
+        <Skeleton className="h-4 w-[95%] my-2" />
+        <Skeleton className="h-4 w-[93%] my-2" />
+        <Skeleton className="h-4 w-[90%] my-2" />
+        <Skeleton className="h-4 w-[94%] my-2" />
+        <Skeleton className="h-4 w-[94%] my-2" />
+        <Skeleton className="h-4 w-[90%] my-2" />
+        <Skeleton className="h-4 w-[98%] my-2" />
+        <Skeleton className="h-4 w-[75%] my-2" />
+    </div>)
+  }
